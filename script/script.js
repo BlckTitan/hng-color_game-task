9a579e0ace5e4380927cd24315dbc5278a282c0b
BlckTitan <dorshman406@gmail.com>
2025-02-06 16:42:59 +0100

    feat(script): added new game feature - added start game feature - added reset game feature

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+let score = 0;
+
 // generate random color to display
 const generateColor = () =>{
     // generate random color
@@ -8,13 +10,13 @@ const generateColor = () =>{
     }
     return color
 }
-
+// display random color on the screen
 const displayRandomColorOnScreen = (color) =>{
     // display random color on the screen
     const displayTarget = document.getElementsByClassName("target_color")[0]
     displayTarget.style.backgroundColor = color
 }
-
+// generate color options
 const generateColorOptions = () =>{
     // generate color options
     const targetColor = generateColor()
@@ -23,6 +25,35 @@ const generateColorOptions = () =>{
     return {colorOptions, targetColor}
 }
 
+// get the selected color
+const getSelectedColor = () =>{
+    // get the user selected color
+   const selectedColor = document.getElementsByClassName('option_button')
+    // get the status of the user choice
+   const status = document.getElementsByClassName('status')[0]
+
+    //check if the user selected the correct color    
+   Array.from(selectedColor).forEach((element) => {
+    element.addEventListener('click', function checkChoiceStatus(e){
+        const target_color = document.getElementsByClassName('target_color')[0].style.backgroundColor
+        const selected_color = e.target.style.backgroundColor
+        
+        if(selected_color === target_color){
+            status.innerHTML = 'Correct';
+            status.style.backgroundColor = 'green';
+            scoreboard(1)
+            displayColorOptions()
+        }else{
+            status.innerHTML = 'Wrong';
+            status.style.backgroundColor = 'red';
+            scoreboard(0)
+            displayColorOptions()
+        }
+    })
+   })
+}
+
+// display color options
 const displayColorOptions = () =>{
     let colorOptions = generateColorOptions()
     // get all the color options
@@ -42,4 +73,79 @@ const displayColorOptions = () =>{
         element.style.backgroundColor = buttonColorOptions[index], targetColor
     })
 }
-displayColorOptions()
+
+// update the scoreboard
+const scoreboard = (newScore) =>{
+    const scoreboard = document.getElementsByClassName('scoreboard')[0]
+    score += newScore
+    scoreboard.innerHTML = score
+}
+
+//  reset game
+const resetGame = () =>{
+    score = 0;
+
+    // reset scoreboard
+    const scoreboard = document.getElementsByClassName('scoreboard')[0]
+    scoreboard.innerHTML = 'Score';
+
+     // reset the status
+    const status = document.getElementsByClassName('status')[0]
+    status.innerHTML = 'Status';
+    status.style.backgroundColor = 'dodgerblue';
+
+    // empty target color
+    const displayTarget = document.getElementsByClassName("target_color")[0]
+    displayTarget.style.backgroundColor = '';
+
+    // empty all the color options
+    const colorOptions = [...Array(6).fill(null)]
+
+    // option buttons
+    const optionButtonTarget = document.getElementsByClassName("option_button");
+
+    Array.from(optionButtonTarget).forEach((element, index) => {
+        element.style.backgroundColor = colorOptions[index]
+    })
+
+    // if there is no color to pick from deactivate all option buttons
+    if(displayTarget.style.backgroundColor.length === 0){
+        Array.from(optionButtonTarget).forEach((element) => {
+            // Empty the color
+            element.style.backgroundColor = ''; 
+            // Remove all event listeners
+            element.replaceWith(element.cloneNode(true));
+        })
+    }
+    
+}
+
+const newGame = () =>{
+    displayColorOptions()
+    getSelectedColor()
+}
+
+
+// start the game
+const startGame = () =>{
+    const displayTarget = document.getElementsByClassName("target_color")[0]
+    const startButton = document.getElementsByClassName('new_game')[0]
+    const instructionText = document.getElementsByClassName('instruction_text')[0]
+
+    startButton.addEventListener('click', () => {
+        (displayTarget.style.backgroundColor.length === 0) ?
+        (
+            newGame(),
+            instructionText.innerHTML = ''
+        )
+         : (
+            resetGame(),
+            instructionText.innerHTML = 'Click New Game to Begin'
+        )
+        
+    })
+
+    
+}
+
+startGame()
